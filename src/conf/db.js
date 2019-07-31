b115f130989b32442c1be5ddacfd1f302e20261d
James's MacBook <jamessupermail@163.com>
2019-08-01 00:23:40 +0800

    [new feats] add redis service

--- src/conf/db.js
+++ src/conf/db.js
@@ -2,18 +2,27 @@ const env = process.env.NODE_ENV; // 获取环境变量
 
 // 配置
 let MYSQL_CONF;
+let REDIS_CONF;
 
 if (env === "dev") {
+  // mysql
   MYSQL_CONF = {
     host: "localhost",
     user: "root",
     password: "Yesh5201314",
     port: "3306",
     database: "myblog"
+  };
+
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: "127.0.0.1"
   };
 }
 
 if (env === "production") {
+  // mysql
   MYSQL_CONF = {
     host: "localhost",
     user: "root",
@@ -21,8 +30,15 @@ if (env === "production") {
     port: "3306",
     database: "myblog"
   };
+
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: "127.0.0.1"
+  };
 }
 
 module.exports = {
-  MYSQL_CONF
+  MYSQL_CONF,
+  REDIS_CONF
 };
