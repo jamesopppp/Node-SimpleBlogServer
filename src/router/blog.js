d0f6af46e0a01cf2f7afcd03a6c3b0423116df99
James's MacBook <jamessupermail@163.com>
2019-08-01 23:21:28 +0800

    [fix bug] fix some bug

--- src/router/blog.js
+++ src/router/blog.js
@@ -20,10 +20,22 @@ const handleBlogRouter = (req, res) => {
 
   // 获取博客列表
   if (method === "GET" && req.path === "/api/blog/list") {
-    const author = req.query.author || "";
+    let author = req.query.author || "";
     const keyword = req.query.keyword || "";
     // const listData = getList(author, keyword);
     // return new SuccessModel(listData);
+
+    if (req.query.isadmin) {
+      // 管理员界面
+      const loginCheckResult = loginCheck(req);
+      if (loginCheckResult) {
+        // 未登录
+        return loginCheckResult;
+      }
+      // 强制查询自己的博客
+      author = req.session.username;
+    }
+
     const result = getList(author, keyword);
     return result.then(listData => {
       return new SuccessModel(listData);
@@ -48,7 +60,7 @@ const handleBlogRouter = (req, res) => {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
       // 未登录
-      return loginCheck;
+      return loginCheckResult;
     }
 
     req.body.author = req.session.username;
@@ -63,7 +75,7 @@ const handleBlogRouter = (req, res) => {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
       // 未登录
-      return loginCheck;
+      return loginCheckResult;
     }
 
     const result = updateBlog(id, req.body);
@@ -81,7 +93,7 @@ const handleBlogRouter = (req, res) => {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
       // 未登录
-      return loginCheck;
+      return loginCheckResult;
     }
 
     const author = req.session.username;
