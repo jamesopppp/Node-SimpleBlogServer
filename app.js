fb1c80f48ab8b206b98bfe149b5f289b7443f4c1
James's MacBook <jamessupermail@163.com>
2019-08-01 14:43:02 +0800

    [new feats] complete all interface

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const querystring = require("querystring");
+const { get, set } = require("./src/db/redis");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 
@@ -10,8 +11,8 @@ const getCookieExpires = () => {
   return d.toGMTString();
 };
 
-// session 数据
-const SESSION_DATA = {};
+// // session 数据
+// const SESSION_DATA = {};
 
 // 用于处理 post data
 const getPostData = req => {
@@ -63,72 +64,98 @@ const serverHandle = (req, res) => {
     req.cookie[key] = val;
   });
 
-  // 解析 session
+  // // 解析 session
+  // let needSetCookie = false;
+  // let userId = req.cookie.userid;
+  // if (userId) {
+  //   if (!SESSION_DATA[userId]) {
+  //     SESSION_DATA[userId] = {};
+  //   }
+  // } else {
+  //   needSetCookie = true;
+  //   userId = `${Date.now()}_${Math.random()}`;
+  //   SESSION_DATA[userId] = {};
+  // }
+  // req.session = SESSION_DATA[userId];
+
+  // 解析 session （使用 redis）
   let needSetCookie = false;
   let userId = req.cookie.userid;
-  if (userId) {
-    if (!SESSION_DATA[userId]) {
-      SESSION_DATA[userId] = {};
-    }
-  } else {
+  if (!userId) {
     needSetCookie = true;
     userId = `${Date.now()}_${Math.random()}`;
-    SESSION_DATA[userId] = {};
+    // 初始化 redis 中的 session 值
+    set(userId, {});
   }
-  req.session = SESSION_DATA[userId];
+  // 获取 session
+  req.sessionId = userId;
+  get(req.sessionId)
+    .then(sessionData => {
+      if (sessionData == null) {
+        // 初始化 redis 中的 session 值
+        set(req.sessionId, {});
+        // 设置 session
+        req.session = {};
+      } else {
+        // 设置 session
+        req.session = sessionData;
+      }
+      console.log("req.session ", req.session);
 
-  // 处理 post data
-  getPostData(req).then(postData => {
-    req.body = postData;
+      // 处理 post data
+      return getPostData(req);
+    })
+    .then(postData => {
+      req.body = postData;
 
-    // 处理 blog 路由
-    // const blogData = handleBlogRouter(req, res);
-    // if (blogData) {
-    //   res.end(JSON.stringify(blogData));
-    //   return;
-    // }
-    const blogResult = handleBlogRouter(req, res);
-    if (blogResult) {
-      blogResult.then(blogData => {
-        if (needSetCookie) {
-          res.setHeader(
-            "Set-Cookie",
-            `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()} `
-          );
-        }
+      // 处理 blog 路由
+      // const blogData = handleBlogRouter(req, res);
+      // if (blogData) {
+      //   res.end(JSON.stringify(blogData));
+      //   return;
+      // }
+      const blogResult = handleBlogRouter(req, res);
+      if (blogResult) {
+        blogResult.then(blogData => {
+          if (needSetCookie) {
+            res.setHeader(
+              "Set-Cookie",
+              `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()} `
+            );
+          }
 
-        res.end(JSON.stringify(blogData));
-      });
-      return;
-    }
+          res.end(JSON.stringify(blogData));
+        });
+        return;
+      }
 
-    // 处理 user 路由
-    // const userData = handleUserRouter(req, res);
+      // 处理 user 路由
+      // const userData = handleUserRouter(req, res);
 
-    // if (userData) {
-    //   res.end(JSON.stringify(userData));
-    //   return;
-    // }
-    const userResult = handleUserRouter(req, res);
-    if (userResult) {
-      userResult.then(userData => {
-        if (needSetCookie) {
-          res.setHeader(
-            "Set-Cookie",
-            `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()} `
-          );
-        }
+      // if (userData) {
+      //   res.end(JSON.stringify(userData));
+      //   return;
+      // }
+      const userResult = handleUserRouter(req, res);
+      if (userResult) {
+        userResult.then(userData => {
+          if (needSetCookie) {
+            res.setHeader(
+              "Set-Cookie",
+              `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()} `
+            );
+          }
 
-        res.end(JSON.stringify(userData));
-      });
-      return;
-    }
+          res.end(JSON.stringify(userData));
+        });
+        return;
+      }
 
-    // 未命中路由，返回 404
-    res.writeHead(404, { "Content-type": "text/plain" });
-    res.write("404 Not Found\n");
-    res.end();
-  });
+      // 未命中路由，返回 404
+      res.writeHead(404, { "Content-type": "text/plain" });
+      res.write("404 Not Found\n");
+      res.end();
+    });
 };
 
 module.exports = serverHandle;
--- src/router/blog.js
+++ src/router/blog.js
@@ -7,6 +7,13 @@ const {
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
 
+// 统一的登录验证函数
+const loginCheck = req => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel("尚未登录"));
+  }
+};
+
 const handleBlogRouter = (req, res) => {
   const method = req.method; // GET POST
   const id = req.query.id;
@@ -38,7 +45,13 @@ const handleBlogRouter = (req, res) => {
     // const data = newBlog(req.body);
     // return new SuccessModel(data);
 
-    req.body.author = "superman"; // 待开发，假数据
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
+    req.body.author = req.session.username;
     const result = newBlog(req.body);
     return result.then(data => {
       return new SuccessModel(data);
@@ -47,6 +60,12 @@ const handleBlogRouter = (req, res) => {
 
   // 更新一篇博客
   if (method === "POST" && req.path === "/api/blog/update") {
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
     const result = updateBlog(id, req.body);
     return result.then(val => {
       if (val) {
@@ -59,7 +78,13 @@ const handleBlogRouter = (req, res) => {
 
   // 删除一篇博客
   if (method === "POST" && req.path === "/api/blog/del") {
-    const author = "superman"; // 待开发，假数据
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheck;
+    }
+
+    const author = req.session.username;
     const result = delBlog(id, author);
     return result.then(val => {
       if (val) {
